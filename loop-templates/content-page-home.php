998e97251ecd4cd549a5644971c1927cffdf6f57
Tom Woodward <twwoodward@me.com>
2020-08-03 15:36:10 -0400

    primary and secondary messaging

--- loop-templates/content-page-home.php
+++ loop-templates/content-page-home.php
@@ -10,6 +10,53 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+	
+	<div class="row lead-row">
+		<div class="col-md-7">
+			<h1><?php the_field('lead_message');?></h1>
+			<div class="lead-detail">
+				<?php the_field('lead_details');?>
+			</div>
+			<div class="get-started-buttons">
+				<h2>get started as</h2>
+				<?php
+				$html = '';
+					// Check rows exists.
+					if( have_rows('started_buttons') ):
+
+					    // Loop through rows.
+					    while( have_rows('started_buttons') ) : the_row();
+
+					        // Load sub field value.
+					        $text = get_sub_field('button_text');
+					        $link = get_sub_field('button_link');
+					        // Do something...
+					        $html .= '<a class="started-buttons" href="' . $link . '">' . $text . '</a>';
+					    // End loop.
+					    endwhile;
+
+					// No value.
+					else :
+					    // Do something...
+					endif;
+					echo $html;
+					?>
+			</div>
+		</div>
+		<div class="col-md-5 teacher-scene"></div>
+	</div>
+	<!--end lead row-->
+
+	<div class="row secondary-row">
+		<div class="col-md-7">
+			<h2><?php the_field('second_message');?></h2>
+			<div class="secondary-detail">
+				<?php the_field('second_message_details');?>
+			</div>
+		</div>
+		<div class="col-md-7">
+		</div>
+	</div>
 
 	<div class="entry-content">
 
